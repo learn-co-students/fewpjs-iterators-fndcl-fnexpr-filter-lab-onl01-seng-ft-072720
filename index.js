6abb9029180088f6033231bd4ba141fd960209a0
acoomes33 <github email address>
2020-11-06 15:37:54 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, string){
+    let downcase = string.toLowerCase()
+    let matches = drivers.filter(n => {return n.toLowerCase() === downcase})
+    return matches
+}
+
+function fuzzyMatch(drivers, string){
+    let matches = drivers.filter(n => {
+        return n.startsWith(string)
+    })
+    return matches
+}
+
+function matchName(drivers, string){
+    let matches = drivers.filter(n => {
+        return n.name === string
+    })
+    return matches
+}
