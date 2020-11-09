3236e277d32c1ab591b6a8758821bc1c6da2588f
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-11-09 13:58:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 // Code your solution here
 
 function findMatching(array, name) {
-    return array.filter( n => {
-        if (n.toLowerCase() === name.toLowerCase()) {
-            return name;
-        }
-    })
+    return array.filter( n => n.toLowerCase() === name.toLowerCase()) 
 }
 
 function fuzzyMatch(array, name) {
-    return array.filter( n => {
-        if (n.slice(0, name.length).toLowerCase() === name.toLowerCase()) {
-            return name;
-        }
-    })
+    return array.filter( n => n.slice(0, name.length).toLowerCase() === name.toLowerCase()) 
 }
 
 function matchName(array, name) {
-    return array.filter( obj => {
-        if (obj.name === name) {
-            return obj
-        }
-    })
+    return array.filter( obj => (obj.name === name))
 }
