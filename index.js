e8a4aeecc103873f8c7862f3815cbc4ec321257f
NickSpangler <Spangleyman@mac.com>
2020-11-09 10:31:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching(array, string) {
+    return array.filter(n => {
+        return n === string || n.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    let test = new RegExp(`^${string}`, 'i')
+    return drivers.filter(n => {
+        return test.test(n)
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(n => {
+        return n.name === string
+    })
+}
