ccc5032ce09f4ba190fc00463806c08d4e9f2584
trevismcgill <trevismcgill25@gmail.com>
2020-11-09 10:00:15 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+function fuzzyMatch(array, string) {
+    let dList = array.filter(d => {
+        return d[0] === string[0]
+    })
+    return dList
+}
+
+function matchName(array, string) {
+    let dList = array.filter(d => {
+        return d.name === string
+    })
+    return dList
+}
+
+function findMatching(array, string) {
+    let dList = array.filter(d => {
+        return d.toLowerCase() === string.toLowerCase()
+    })
+    return dList
+}
