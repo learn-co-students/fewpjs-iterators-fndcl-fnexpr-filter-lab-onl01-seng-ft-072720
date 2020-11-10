c637addaf2351d3152da7515bd249dca40226908
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-11-10 02:47:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 function findMatching(drivers, str){
-   return drivers.filter(name => {
-      return name === str
-  })
+   return drivers.filter(name => 
+      name.toLowerCase() === str.toLowerCase()
+  )
 }
 
 function fuzzyMatch(drivers, str){
-  return drivers.filter(name => {
-    return name.startsWith(str)
-  })
+  return drivers.filter(name => 
+     name.startsWith(str)
+  )
 }
 
 function matchName(drivers, str) {
-  return drivers.filter(obj => {
-    return obj.name === str 
-  })
+  return drivers.filter(obj => 
+     obj.name === str 
+  )
 }
+
